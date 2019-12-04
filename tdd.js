23e67e9fe131c80671039d6d6e74fc80d86b4517
Paul Montgomery <paul.montgomery@PRFT-11395-MAC.local>
2019-12-04 14:26:28 -0600

    postgres looks neat

---
+++ tdd.js
@@ -0,0 +1,3 @@
+const d1 = new Date().getTime() 
+const d2 = new Date().getTime() 
+console.log( d2 - d1 )
---
+++ xyz.js
@@ -0,0 +1,6 @@
+function myFunc() {
+    console.log("Hello from myFunc")
+}
+ module.exports = {
+	myFunc
+} 
